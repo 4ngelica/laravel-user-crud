5102d1731e98b68c063c24183550ab6f029790f2
Angélica Batassim <angelicabatassim@gmail.com>
2021-07-09 17:16:12 -0300

    domain fix

--- config/session.php
+++ config/session.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'laravel-user-crud.herokuapp.com/'),
+    'domain' => env('SESSION_DOMAIN', 'laravel-user-crud.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
